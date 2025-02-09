f9dac224a4e4593af7031ed66c0f271e55e6bc47
yunfengsg <yunfengsg@gmail.com>
2025-02-09 17:57:51 +0800

    fix issue

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 terraform {
+    required_version = ">= 1.0" 
   backend "s3" {
     bucket = "sctp-ce8-tfstate"
     key    = "yyf-project-s3-tf-ci.tfstate"  #Change this
@@ -12,10 +13,7 @@ terraform {
 
 data "aws_caller_identity" "current" {}
 
-locals {
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
-  account_id  = "${data.aws_caller_identity.current.account_id}"
-}
+
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
